a1fc0ae1a5c47c0fc8aacc222f6427297e643868
ZEESHAN  MALIK <zeeshanmalik.ast1214@gmail.com>
2026-08-24 23:59:14 +0500

    i writesome commants
    i

--- vectorPushBack.cpp
+++ vectorPushBack.cpp
@@ -7,7 +7,7 @@ int main(){
     vec.push_back(1);
     vec.push_back(2);
     vec.push_back(3);
-    vec.push_back(4);
+    vec.push_back(4);// this is push back
     vec.push_back(5);
     vec.push_back(6);
     vec.push_back(7);
