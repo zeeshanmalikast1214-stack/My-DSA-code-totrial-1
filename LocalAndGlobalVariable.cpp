b255e79b2c15851a5d3d820df6a230fe82955363
ZEESHAN  MALIK <zeeshanmalik.ast1214@gmail.com>
2026-08-25 23:07:05 +0500

    full concept of  global variable

---
+++ LocalAndGlobalVariable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+     int y=10;
+    {
+
+    int x=5;
+    cout<<"the value of X  inside bracket is   : "<<x<<endl;
+}
+    cout<<"the value of X is outside bracket    : "<<y<<endl;
+    return 0;
+}
